358521baf96f97985ff6dde2dcff9f2ca03eba13
Navid Heydari <navidh2@uw.edu>
2018-05-27 13:20:27 -0700

    Adjusted Script for num=20 and call the function 200 times. results added

--- taskExecutor.sh
+++ taskExecutor.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
+module load parallel
+
 echo "starting"
 
-for ((count=1;count < 200;count++))
+for ((count=1;count < 200 ;count++))
 {
-	curl https://openwhisk.ng.bluemix.net/api/v1/web/navidh2%40uw.edu_dev/default/openWhiskTester.json?number=50 | grep result >> cpuStress_res.csv
+	#curl https://openwhisk.ng.bluemix.net/api/v1/web/navidh2%40uw.edu_dev/default/openWhiskTester.json?number=20 | grep result > cpuStress_res${count}.csv
+
+curl https://openwhisk.ng.bluemix.net/api/v1/web/navidh2%40uw.edu_dev/default/openWhiskTester.json?number=20 | grep result >> cpuStress_res.csv
+
 }
 echo "finished"
 exit 0
